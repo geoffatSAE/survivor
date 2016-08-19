90b8de14dd690d773b1bd8142cb65a91f46b7313
Qxintero <1005829@student.sae.edu.au>
2016-08-19 20:17:08 +1000

    Levels one, two and three (Easy, Medium and Hard).

---
+++ Assets/Resources/Classes/PlayerClass.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerClass : MonoBehaviour
+{
+    Rigidbody rb;
+    private bool hitGround;
+    private float horrizontalMomentum;
+    private float verticalMomentum;
+    private Vector2 momentum;
+    private float timer;
+    private int health;
+    private float gravity = 1.30f;
+    public GameObject gameOver;
+
+    public Vector3 minimum;
+    public Vector3 maximum;
+
+    void Start ()
+	{
+        hitGround = true;
+        rb = transform.GetComponent<Rigidbody>();
+        health = 25;
+	}
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) && hitGround)
+        {
+            verticalMomentum = 10;
+
+        }else if (Input.GetKey(KeyCode.S) && !hitGround)
+        {
+            verticalMomentum -= 24 * Time.deltaTime * gravity;
+        }
+
+        if (!hitGround)
+        {
+            verticalMomentum -= 12 * Time.deltaTime * gravity;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, -90, 0);
+            if (horrizontalMomentum > -0.4f)
+            {
+                horrizontalMomentum = -0.4f;
+            }
+            horrizontalMomentum -= 8*Time.deltaTime * gravity;
+        }else if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 90, 0);
+            if(horrizontalMomentum < 0.4f)
+            {
+                horrizontalMomentum = 0.4f;
+            }
+            horrizontalMomentum += 8 * Time.deltaTime * gravity;
+        }else
+        {
+            horrizontalMomentum /= 1.5f;
+        }
+
+        if (horrizontalMomentum > 4)
+        {
+            horrizontalMomentum = 4;
+        }
+        else if (horrizontalMomentum < -4)
+        {
+            horrizontalMomentum = -4;
+        }
+
+        if (rb.velocity.y > -0.01 && rb.velocity.y < 0.01 && !hitGround)
+        {
+            if (verticalMomentum > 0)
+            {
+                verticalMomentum = 0;
+            }
+
+            if(verticalMomentum < 0)
+            {
+                verticalMomentum = 0;
+            }
+        }
+
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            Color setColor = new Color(-timer*2, -timer*3.5f +1, -timer*2 +1, 1);
+            transform.GetComponent<Renderer>().material.color = setColor;
+            if(timer - 0.5f > 0)
+            {
+                transform.localScale = new Vector3(((timer - 0.5f) / 3) + 0.5f, -((timer-0.5f) / 3) + 0.5f, ((timer - 0.5f) / 3) + 0.5f);
+            }
+        }
+
+        momentum = new Vector2(horrizontalMomentum, verticalMomentum);
+        rb.velocity = momentum;
+
+        if(verticalMomentum > 0)
+        {
+            hitGround = false;
+        }
+
+        Vector3 Pos = transform.position;
+        Pos.x = Mathf.Clamp(Pos.x, minimum.x, maximum.x);
+        Pos.y = Mathf.Clamp(Pos.y, minimum.y, maximum.y);
+        Pos.z = Mathf.Clamp(Pos.z, minimum.z, maximum.z);
+    }
+
+    void OnTriggerEnter()
+    {
+        hitGround = true;
+        verticalMomentum = 0;
+        timer = 0.75f;
+    }
+    void OnTriggerExit()
+    {
+        hitGround = false;
+        if (verticalMomentum == 0)
+        {
+            verticalMomentum = -1.5f;
+        }
+    }
+
+    public void Damage(int dmg)
+    {
+        health -= dmg;
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+            gameOver.SetActive(true);
+        }
+    }
+}
